f33c6348c2b4870b1153fe40d6c04312b1667d7c
aniekan7132-git <saniekan32@gmail.com>
2023-01-19 16:04:06 +0100

    AJAX call using XMLHttpRequest

--- script.js
+++ script.js
@@ -718,7 +718,6 @@ console.log(acc1.getMovements());
 acc1.deposit(300).deposit(500).withdrawal(35).requestLoan(25000).deposit(4000);
 console.log(acc1.getMovements());*/
 
-
 // A CLOSER LOOK AT FUNCTIONS
 
 /*const bookings = [];
@@ -919,7 +918,7 @@ console.log(addVAT2(100));
 console.log(addVAT(23));*/
 
 ///////////////////////////////////
-// IIFE 
+// IIFE
 /*(function () {
   console.log("This function will never run again");
 })();
@@ -1148,86 +1147,56 @@ console.log(h1.parentElement.children);
 });*/
 
 // Slider
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left')
-const btnRight = document.querySelector('.slider__btn--right');
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
 
 let currentSlide = 0;
 
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.2)';
-slider.style.overflow = 'visible';
+const slider = document.querySelector(".slider");
+slider.style.transform = "scale(0.4) translateX(-800px)";
+slider.style.overflow = "visible";
 
 slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
 // 0%, 100%, 200%, 300%
 
 // Next slide
-btnRight.addEventListener('click', function () {
-  currentSlide++
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btnRight.addEventListener("click", function () {
+  currentSlide++;
+
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - currentSlide)}%)`)
+  );
+});
+
+const getCountryData = function (country) {
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `
+ <article class="country">
+   <img class="country__img" src="${data.flags.png}" />
+   <div class="country__data">
+     <h3 class="country__name">${data.name.common}</h3>
+     <h4 class="country__region">${data.region}</h4>
+     <p class="country__row"><span>👫</span>${(
+       +data.population / 1000000
+     ).toFixed(1)} people</p>
+     <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+     <p class="country__row"><span>💰</span>${data.currencies.NGN.name}</p>
+   </div>
+ </article>
+ `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  countriesContainer.style.opacity = 1;
+});
+};
+
+getCountryData('nigeria');
+//getCountryData('usa');
